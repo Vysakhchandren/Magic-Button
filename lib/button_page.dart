bbd7c10d2833456c025a9a269e45c9e5589d2964
Vysakhchandren <vysakhc14@gmail.com>
2025-11-08 15:57:33 +0530

    Final commit

--- lib/button_page.dart
+++ lib/button_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class ButtonPage extends StatefulWidget {
   const ButtonPage({super.key});
@@ -8,22 +9,38 @@ class ButtonPage extends StatefulWidget {
 }
 
 class _ButtonPageState extends State<ButtonPage> {
+  int ballNumber = 0;
+  List<String> list = ['Yes', 'No', 'Maybe', 'Probably', "Don't think so"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.amber,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                minimumSize: const Size(200, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 0;
+                });
+                print('I got clicked for ball numer $ballNumber');
+                
+              },
+              child: Text('PRESS',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 50),),
             ),
           ),
-          onPressed: () {
-            Text('I got clicked');
-          },
-          child: null,
-        ),
-
+          SizedBox(height: 20,),
+          Text(list[ballNumber],style: TextStyle(fontSize: 30),)
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:magic_button/button_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
 
-      home: BackButton(),
+      home: ButtonPage(),
     );
   }
 }
